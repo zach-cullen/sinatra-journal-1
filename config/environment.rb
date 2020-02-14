65adc1cefc7fe7ebb116e38aa4624449019a373f
Zach Cullen <zacullen@gmail.com>
2020-02-14 09:20:54 -0500

    reorder bundler statement

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,14 @@
-require 'bundler'
-#load all ruby gems
-Bundler.require
-
 ENV['SINATRA_ENV'] ||= 'development'
+
+#load all ruby gems
+require 'bundler'
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
 #create and connect to database
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
-
 
 #load all app files NOTE: gem does not respect order, require dependencies first
 require_all 'app'
