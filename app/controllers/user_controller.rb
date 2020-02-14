23700d23908a7081cda24d8c05994cacc7ac55b4
Zach Cullen <zacullen@gmail.com>
2020-02-14 08:51:19 -0500

    comment descriptions of user routes

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,38 @@
+class UserController < Sinatra::Base
+
+
+  get '/signup' do 
+    #create new user
+    #user entered username
+    #user entered email
+    #user entered password
+    #username is unique
+    #email is unique
+    #send to login
+  end
+
+  get '/login' do
+    #user email is in database
+    #user password matches 
+    #create session user_id
+    #send to user/:slug
+  end
+
+  get '/user/:slug' do
+
+  end
+
+  get '/logout' do
+    #clear session
+    #send to home page
+  end
+
+  get '/failure' do
+    #send to login w/ message
+  end 
+
+  get '/delete' do
+    #delete user
+  end
+
+end
