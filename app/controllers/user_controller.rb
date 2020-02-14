d829989e47e98460b8b7f6ec9d1c8702070cdcca
Zach Cullen <zacullen@gmail.com>
2020-02-14 12:13:07 -0500

    check username format and add flash message

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,11 +3,15 @@ class UsersController < ApplicationController
   get '/signup' do 
     #render signup form
     erb :signup
-
   end
 
   post '/signup' do
-    binding.pry
+    @user_info = params[:user]
+    if !username_format_valid?(@user_info[:username])
+      redirect '/signup'
+    else
+      flash[:alert] = "username looks good."
+    end
     #user entered username w/ only acceptable characters
     #user entered valid email
     #user entered password
@@ -39,6 +43,31 @@ class UsersController < ApplicationController
 
   get '/delete' do
     #delete user
+  end
+
+  helpers do 
+    def username_format_valid?(string)
+      valid_format = true
+      message = nil
+      if string.length <= 6
+        valid_format = false
+        flash[:alert] = "username must be at least 6 characters"
+      elsif /\A\w[^_]*\z/.match(string).to_s != string
+        valid_format = false
+        flash[:alert] = "username may only contain letters and numbers"
+      end
+      valid_format
+    end
+    
+    def email_format_valid?(string)
+
+    end
+
+    def password_format_valid?(string)
+
+    end
+
+
   end
 
 end
