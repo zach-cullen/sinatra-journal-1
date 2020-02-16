611ad4846a5b07a7dd843100a224df30a7b5320a
Zach Cullen <zacullen@gmail.com>
2020-02-16 10:45:58 -0500

    give post route ability to create new journal and associate to user

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -1,12 +1,24 @@
 class JournalsController < ApplicationController
 
   get '/journals/:username' do
+    @user = find_user_or_logout
     #list all journals created by user
   end
 
   post '/journals/:username' do
+    @user = find_user_or_logout
     binding.pry
     #create new journal at user
+    @journal = @user.journals.create(params[:journal])
+    #redirect to show page
+    binding.pry
+    redirect "/journals/#{@user.username}/#{@journal.id}"
+  end
+
+  get '/journals/:username/:id' do 
+    @user = find_user_or_logout
+    @journal = Journal.find(params[:id])
+    erb :show
   end
 
 end
