4b367992a4651931f90829a48978363ea3b5f684
Zach Cullen <zacullen@gmail.com>
2020-02-16 12:43:17 -0500

    change reference to find journal method

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -2,7 +2,7 @@ class PromptsController < ApplicationController
 
   post '/prompts/:username/:journal_id' do 
     @user = find_user_or_logout
-    @journal = find_journal_or_logout
+    @journal = find_journal_or_redirect
   end
 
 end
