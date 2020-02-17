bf86f0dce97aeb2ca023c27eb4f895b99ff5ef0d
Zach Cullen <zacullen@gmail.com>
2020-02-17 09:47:13 -0500

    make prompt post route create new prompt and redirect

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -3,6 +3,8 @@ class PromptsController < ApplicationController
   post '/prompts/:username/:journal_id' do 
     @user = find_user_or_logout
     @journal = find_journal_or_redirect
+    new_prompt = @journal.prompts.create(params[:prompt])
+    redirect "/journals/#{@user.username}/#{@journal.id}/write"
   end
 
 end
