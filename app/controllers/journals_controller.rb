a4b1985ea526d8e6afa86c57c799af5688fcfd49
Zach Cullen <zacullen@gmail.com>
2020-02-17 09:10:08 -0500

    remove pry bindings for create journal

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -2,11 +2,9 @@ class JournalsController < ApplicationController
 
   post '/journals/:username' do
     @user = find_user_or_logout
-    binding.pry
     #create new journal at user
     @journal = @user.journals.create(params[:journal])
     #redirect to show page
-    binding.pry
     redirect "/journals/#{@user.username}/#{@journal.id}"
   end
 
