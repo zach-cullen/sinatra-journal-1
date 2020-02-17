3750d10fe661e0035d8216728907182f998680ee
Zach Cullen <zacullen@gmail.com>
2020-02-17 11:09:58 -0500

    fix typo in entries association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :journals
   has_many :prompts, through: :journals
-  has_many :entires, through: :prompts
+  has_many :entries, through: :prompts
 end
