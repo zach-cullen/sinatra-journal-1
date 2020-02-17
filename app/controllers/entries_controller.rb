a1c5683eaa39379b4df1866816edd3183f448f2b
Zach Cullen <zacullen@gmail.com>
2020-02-17 11:08:04 -0500

    create entries controller and post route for creating new entry

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,12 @@
+class EntriesController < ApplicationController
+
+  post '/entries/:username/:journal_id/:prompt_id' do 
+    @user = find_user_or_logout
+    @journal = find_journal_or_redirect
+    @prompt = @journal.prompts.find_by(id: params[:prompt_id])
+    new_entry = @prompt.entries.create(params[:text])
+    binding.pry
+    redirect "/journals/#{@user.username}/#{@journal.id}/write"
+  end
+
+end
