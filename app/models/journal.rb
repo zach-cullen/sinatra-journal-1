c9fbb7af724bbc9ae185bbc62e925c6831e25a87
Zach Cullen <zacullen@gmail.com>
2020-02-17 10:36:25 -0500

    build association with entry model

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,4 +1,5 @@
 class Journal < ActiveRecord::Base
   belongs_to :user
   has_many :prompts
+  has_many :entries, through: :prompts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :journals
   has_many :prompts, through: :journals
+  has_many :entires, through: :prompts
 end
