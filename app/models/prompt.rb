b502e54581e044aa7bbe88ee80ecf0ec4636e975
Zach Cullen <zacullen@gmail.com>
2020-02-19 11:44:23 -0500

    add method entry_made_today

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,4 +1,9 @@
 class Prompt < ActiveRecord::Base
   belongs_to :journal
   has_many :entries, :dependent => :destroy
+
+  def entry_made_today
+    #returns true or false if an entry was already made today for this prompt
+    self.journal.entries_made_today.find{|e| e.prompt == self}
+  end
 end
