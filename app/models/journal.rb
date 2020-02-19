09da310ef6ffe47527042dadb81ab036fd81c425
Zach Cullen <zacullen@gmail.com>
2020-02-19 11:02:59 -0500

    create method entries_made_today_in_localtime

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -2,4 +2,11 @@ class Journal < ActiveRecord::Base
   belongs_to :user
   has_many :prompts, :dependent => :destroy
   has_many :entries, through: :prompts
+
+  def entries_made_today_in_localtime
+    #returns number of entries made in journal today or nil
+    today_localtime = Time.now.getlocal.to_date
+    entries = self.entries.select{|e| e.date_created_in_localtime == today_localtime}
+    entries ? entries.count : nil
+  end
 end
