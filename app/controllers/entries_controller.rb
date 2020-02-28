34709613d30daea164ef1c3a7b378f7b9054ff1e
Zach Cullen <zacullen@gmail.com>
2020-02-28 12:50:26 -0500

    create route to render edit entry form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -35,9 +35,23 @@ class EntriesController < ApplicationController
     redirect "/journals/#{@journal.id}/write"
   end
 
+  #render for for Updating entry
+  get '/entries/:entry_id/edit' do
+    @entry = Entry.find_by(id: params[:entry_id])
+    @journal = @entry.prompt.journal
+    if logged_in? && journal_access_valid?
+      erb :'entries/edit'
+    elsif !logged_in?
+      flash[:alert] = "You must be logged in to view this content"
+      redirect 'login'
+    else
+      redirect "/user/#{current_user.id}/journals"
+    end
+  end
+
   #Update entry
   patch '/entries/:entry_id' do
-
+    binding.pry
   end 
 
   helpers do 
