bb35e979713620a156dbce8f3f36c357b7948083
Zach Cullen <zacullen@gmail.com>
2020-02-28 11:21:52 -0500

    delete replaced bad code

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,12 +1,5 @@
 class PromptsController < ApplicationController
 
-  # post '/prompts/:username/:journal_id' do 
-  #   @user = find_user_or_logout
-  #   @journal = find_journal_or_redirect
-  #   new_prompt = @journal.prompts.create(params[:prompt])
-  #   redirect "/journals/#{@user.username}/#{@journal.id}/write"
-  # end
-
   #Create new prompt
   post '/prompts' do
     @journal = Journal.find_by(id: params[:journal][:id].to_i)
