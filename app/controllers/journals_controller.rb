59be95e3fa57beace31224716269753b7e1c4a91
Zach Cullen <zacullen@gmail.com>
2020-02-28 10:15:20 -0500

    validate journal access before redirecting to read page

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,31 +1,50 @@
 class JournalsController < ApplicationController
 
   post '/journals/:username' do
-    @user = find_user_or_logout
-    #create new journal at user
-    @journal = @user.journals.create(params[:journal])
-    #redirect to show page
-    redirect "/journals/#{@user.username}/#{@journal.id}/write"
+    # @user = find_user_or_logout
+    # #create new journal at user
+    # @journal = @user.journals.create(params[:journal])
+    # #redirect to show page
+    # redirect "/journals/#{@user.username}/#{@journal.id}/write"
   end
 
   get '/journals/:journal_id' do 
-    @user = find_user_or_logout
-    @journal = find_journal_or_redirect
-    erb :'journals/read'
+    # @user = find_user_or_logout
+    # @journal = find_journal_or_redirect
+    @user = current_user
+    @journal = Journal.find_by(id: params[:journal_id])
+    if logged_in? && journal_access_valid?
+      erb :'journals/read'
+    elsif logged_in?
+      redirect "user/#{@user.id}/journals"
+    else
+      flash[:alert] = "You must be logged in to view this content"
+      redirect 'login'
+    end
   end
 
 
   get '/journals/:journal_id/write' do 
-    @user = find_user_or_logout
-    @journal = find_journal_or_redirect
+    # @user = find_user_or_logout
+    # @journal = find_journal_or_redirect
     erb :'journals/write'
   end
 
   delete '/journals/:username/:journal_id' do
-    @user = find_user_or_logout
-    @journal = find_journal_or_redirect
-    @journal.destroy
-    redirect "/user/#{@user.username}"
+    # # @user = find_user_or_logout
+    # # @journal = find_journal_or_redirect
+    # @journal.destroy
+    # redirect "/user/#{@user.username}"
   end
 
+  helpers do 
+    # make sure the journal exists and that it belongs to the current user
+    def journal_access_valid?
+      if !!@journal && @journal.user == current_user
+        true
+      else
+        false
+      end
+    end
+  end
 end
