a49a4a843feb829f50d651e331e28bb1e9ebca0b
Zach Cullen <zacullen@gmail.com>
2020-02-28 11:23:24 -0500

    comment out bad code for rewriting

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,26 +1,26 @@
 class EntriesController < ApplicationController
 
-  post '/entries/:username/:journal_id/:prompt_id' do 
-    @user = find_user_or_logout
-    @journal = find_journal_or_redirect
-    @prompt = @journal.prompts.find_by(id: params[:prompt_id])
-    new_entry = @prompt.entries.create(params[:entry])
-    redirect "/journals/#{@user.username}/#{@journal.id}/write"
-  end
+  # post '/entries/:username/:journal_id/:prompt_id' do 
+  #   @user = find_user_or_logout
+  #   @journal = find_journal_or_redirect
+  #   @prompt = @journal.prompts.find_by(id: params[:prompt_id])
+  #   new_entry = @prompt.entries.create(params[:entry])
+  #   redirect "/journals/#{@user.username}/#{@journal.id}/write"
+  # end
 
-  get '/entries/:username/:journal_id/:entry_id/edit' do
-    @user = find_user_or_logout
-    @journal = find_journal_or_redirect
-    @entry = Entry.find_by(id: params[:entry_id])
-    erb :'journals/edit-entry'
-  end
+  # get '/entries/:username/:journal_id/:entry_id/edit' do
+  #   @user = find_user_or_logout
+  #   @journal = find_journal_or_redirect
+  #   @entry = Entry.find_by(id: params[:entry_id])
+  #   erb :'journals/edit-entry'
+  # end
 
-  patch '/entries/:username/:journal_id/:entry_id' do
-    @user = find_user_or_logout
-    @journal = find_journal_or_redirect
-    @entry = Entry.find_by(id: params[:entry_id])
-    @entry.update(params[:entry])
-    redirect "/journals/#{@user.username}/#{@journal.id}/write"
-  end
+  # patch '/entries/:username/:journal_id/:entry_id' do
+  #   @user = find_user_or_logout
+  #   @journal = find_journal_or_redirect
+  #   @entry = Entry.find_by(id: params[:entry_id])
+  #   @entry.update(params[:entry])
+  #   redirect "/journals/#{@user.username}/#{@journal.id}/write"
+  # end
 
 end
