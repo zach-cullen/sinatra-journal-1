ad71976453825869f067eebe563343493fa990eb
Zach Cullen <zacullen@gmail.com>
2020-02-28 11:19:58 -0500

    create new post route to create prompt and validate journal access

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -7,4 +7,28 @@ class PromptsController < ApplicationController
   #   redirect "/journals/#{@user.username}/#{@journal.id}/write"
   # end
 
+  #Create new prompt
+  post '/prompts' do
+    @journal = Journal.find_by(id: params[:journal][:id].to_i)
+    if logged_in? && journal_access_valid?
+      @journal.prompts.create(params[:prompt])
+    else
+      redirect '/login'
+    end
+    redirect "/journals/#{@journal.id}/write"
+  end
+
+
+  helpers do 
+
+    def journal_access_valid?
+      if !!@journal && @journal.user == current_user
+        true
+      else
+        false
+      end
+    end
+
+  end
+
 end
