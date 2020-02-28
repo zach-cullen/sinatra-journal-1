3f3cdc0fbcb20700764834845489d5bf2e44ba09
Zach Cullen <zacullen@gmail.com>
2020-02-28 13:00:00 -0500

    delete unused bad code

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,28 +1,5 @@
 class EntriesController < ApplicationController
 
-  # post '/entries/:username/:journal_id/:prompt_id' do 
-  #   @user = find_user_or_logout
-  #   @journal = find_journal_or_redirect
-  #   @prompt = @journal.prompts.find_by(id: params[:prompt_id])
-  #   new_entry = @prompt.entries.create(params[:entry])
-  #   redirect "/journals/#{@user.username}/#{@journal.id}/write"
-  # end
-
-  # get '/entries/:username/:journal_id/:entry_id/edit' do
-  #   @user = find_user_or_logout
-  #   @journal = find_journal_or_redirect
-  #   @entry = Entry.find_by(id: params[:entry_id])
-  #   erb :'journals/edit-entry'
-  # end
-
-  # patch '/entries/:username/:journal_id/:entry_id' do
-  #   @user = find_user_or_logout
-  #   @journal = find_journal_or_redirect
-  #   @entry = Entry.find_by(id: params[:entry_id])
-  #   @entry.update(params[:entry])
-  #   redirect "/journals/#{@user.username}/#{@journal.id}/write"
-  # end
-
   #Create new entry
   post '/entries' do
     @prompt = Prompt.find_by(id: params[:prompt][:id].to_i)
